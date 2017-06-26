d5362355c5c9fb20555908dd59f6c89f507abecd
gigassyu <dragonquest.3710@ezweb.ne.jp>
2017-06-26 17:22:20 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,6 +8,15 @@ document.getElementByid('list').appendChild(li);
 document.getElementByid('form').onsubmit = function(){
  window.alert(document.getElementByid('form').word.value);
  return false;
+ var include_tax = function(price){
+   return Math.floor(price + 0.08 * price);
+ }
+ 
+ for(var i = 1; i < 10; i++){
+var li = document.createElement('li');
+var price = Math.floor(Math.random() * 1000);
+li.textContent = price + '円'　+　include_tax(price)
+  document.getElementByid('price').appendChild(li);
  };
 
  
