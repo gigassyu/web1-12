7132639c7a3f4ef682b376c0833a527d8f9f0aa2
gigassyu <dragonquest.3710@ezweb.ne.jp>
2017-06-26 17:39:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,4 @@
-var li = document.createElement('li');
+/*var li = document.createElement('li');
 li.textContent = 'アイテム';
 document.getElementByid('list').appendChild(li);
 for(var i = 1; i < 10; i++){
@@ -11,12 +11,16 @@ document.getElementByid('form').onsubmit = function(){
  var include_tax = function(price){
    return Math.floor(price + 0.08 * price);
  }
- 
- for(var i = 1; i < 10; i++){
+ */
+ for(var i = 1; i < 100; i++){
+  
 var li = document.createElement('li');
+li.textContent
 var price = Math.floor(Math.random() * 1000);
-li.textContent = price + '円'　+　include_tax(price)
+li.textContent = price + '円'　+　include_tax(price) + '円)';
   document.getElementByid('price').appendChild(li);
+  
+  
  };
 
  
